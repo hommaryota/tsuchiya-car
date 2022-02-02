dd965fc886c12f7954b3ae158372664de363f339
Honma Ryota <m95.honma.ryota@gmail.com>
2022-02-02 19:12:55 +0900

    car border section created

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -2,6 +2,13 @@ let mySwiper = new Swiper(".swiper", {
   // 以下にオプションを設定
   loop: true, //最後に達したら先頭に戻る
 
+  // オートリプレイ
+  autoplay: {
+    delay: 3000,
+    stopOnLastSlide: false,
+    disableOnInteraction: false,
+  },
+
   //ページネーション表示の設定
   pagination: {
     el: ".swiper-pagination", //ページネーションの要素
@@ -13,10 +20,5 @@ let mySwiper = new Swiper(".swiper", {
   navigation: {
     nextEl: ".swiper-button-next", //「次へボタン」要素の指定
     prevEl: ".swiper-button-prev", //「前へボタン」要素の指定
-  },
-
-  //スクロールバー表示の設定
-  scrollbar: {
-    // el: ".swiper-scrollbar", //要素の指定
   },
 });
