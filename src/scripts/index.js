632175b925c26c5b70b895c7296f56818d30d1b2
Honma Ryota <m95.honma.ryota@gmail.com>
2022-02-04 11:23:48 +0900

    tab切り替え時のimg表示切り替えjsのみ実装

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -23,3 +23,33 @@ document.querySelector(".company").addEventListener("mouseout", function () {
   document.querySelector(".company__p").classList.remove("hover");
   document.querySelector(".nav__company").classList.remove("hover");
 });
+
+// shop hover
+// .shop__list-liを全て取り出す
+const shopList = document.querySelectorAll(".shop__list-li");
+// addEventListenerを各.shop__list-liに使うためにforEachで繰り返す
+shopList.forEach(function (shopList) {
+  // shopListをホバー時の処理
+  shopList.addEventListener("mouseover", function () {
+    // hoverクラスを取る
+    document.querySelectorAll(".shop__list-li")[0].classList.remove("hover");
+    // hoverした要素にhoverクラスを付与
+    shopList.classList.add("hover");
+  });
+
+  // 以下mouseout時の処理
+  shopList.addEventListener("mouseout", function () {
+    shopList.classList.remove("hover");
+  });
+});
+
+// .shop__list-liクリック時の表示
+document.addEventListener("DOMContentLoaded", function () {
+  for (let i = 0; i < shopList.length; i++) {
+    shopList[i].addEventListener("click", shopClick);
+  }
+  function shopClick() {
+    document.querySelector(".active").classList.remove("active");
+    this.classList.add("active");
+  }
+});
